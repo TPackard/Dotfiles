be72701b26efa01e76b51214a15f23d9cab519f5
Tyler Packard <tyler.packard86@gmail.com>
2016-07-11 16:25:45 -0700

    Remove lemonbar scripts

--- bin/redrawbar.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/zsh
-kill -s SIGINT $(pidof -x bar.sh)
--- bin/runbar.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/zsh
-
-#~/bin/bar.sh | lemonbar -g x32++ -f "basis33:size=12" -o 1 -F "#E0E0E0" -B "#34495E" | zsh
-~/bin/bar.sh | lemonbar -g x32++ -f "Inconsolata:size=10" -o -1 -F "#E0E0E0" -B "#000000" | zsh
